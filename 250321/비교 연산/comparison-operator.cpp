6eab8022a369db689fbc8a101d0fdd7e9b84c8aa
GreedSpirit <147044891+GreedSpirit@users.noreply.github.com>
2025-03-21 20:56:35 +0900

    Title: 비교 연산, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250321/비교 연산/comparison-operator.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int A, B;
+    cin >> A >> B;
+
+    if(A >= B) cout << 1;
+    else cout << 0;
+    cout << '\n';
+    if(A > B) cout << 1;
+    else cout << 0;
+    cout << '\n';
+    if(A <= B) cout << 1;
+    else cout << 0;
+    cout << '\n';
+    if(A < B) cout << 1;
+    else cout << 0;
+    cout << '\n';
+    if(A == B) cout << 1;
+    else cout << 0;
+    cout << '\n';
+    if(A != B) cout << 1;
+    else cout << 0;
+}
