569db3baac8bc0c9b75ee7012ebf837b397df76c
GreedSpirit <147044891+GreedSpirit@users.noreply.github.com>
2025-03-21 20:44:30 +0900

    Title: 세 실수의 반올림, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250321/세 실수의 반올림/rounding-of-three-actual-numbers.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double a, b, c;
+
+    cin >> a >> b >> c;
+
+    cout << fixed;
+    cout.precision(3);
+    cout << a;
+    cout << '\n';
+    cout << b;
+    cout << '\n';
+    cout << c;
+}
