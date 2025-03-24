e462b7b17418c226fad80aeec282142db1c119ea
GreedSpirit <147044891+GreedSpirit@users.noreply.github.com>
2025-03-24 10:55:08 +0900

    Title: 다양하게 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 250324/다양하게 출력/print-in-variety.cpp
+++ 250324/다양하게 출력/print-in-variety.cpp
@@ -2,5 +2,5 @@
 using namespace std;
 
 int main() {
-    cout << "Total days in year\n365\nCirumference rate\n3.1415926535";
+    cout << "Total days in Year\n365\nCircumference rate\n3.1415926535";
 }
