a9ac5c6db058c523173048b675c7e6015cd61c88
GreedSpirit <147044891+GreedSpirit@users.noreply.github.com>
2025-03-25 21:13:23 +0900

    Title: 길이 변형 후 사각형 넓이 구하기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250325/길이 변형 후 사각형 넓이 구하기/square-width-after-length-change.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x, y;
+    cin >> x >> y;
+    x = x + 8;
+    y = y * 3;
+
+    cout << x;
+    cout << '\n';
+    cout << y;
+    cout << '\n';
+    cout << x * y;
+}
