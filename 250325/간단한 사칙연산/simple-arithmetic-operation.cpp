125267b3483e9b7ba23af75bc8745d8636a4fb27
GreedSpirit <147044891+GreedSpirit@users.noreply.github.com>
2025-03-25 20:54:29 +0900

    Title: 간단한 사칙연산, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250325/간단한 사칙연산/simple-arithmetic-operation.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int A, B;
+    cin >> A >> B;
+
+    cout << A + B;
+    cout << '\n';
+    cout << A - B;
+    cout << '\n';
+    cout << A / B;
+    cout << '\n';
+    cout << A % B;
+}
