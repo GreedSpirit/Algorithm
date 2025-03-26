ababc07e68d7e5d2898ec6bd017826dc6d1bafa8
GreedSpirit <147044891+GreedSpirit@users.noreply.github.com>
2025-03-26 09:59:39 +0900

    Title: 물의 상태, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250326/물의 상태/state-of-water.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    if(n >= 100){
+        cout << "vapor";
+    }
+    else if(n < 0) cout << "ice";
+    else cout << "water";
+}
