bef8323390e42793c646a3bd8e0c18ce0e585e92
GreedSpirit <147044891+GreedSpirit@users.noreply.github.com>
2025-03-26 11:34:20 +0900

    Title: 점수 비교, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250326/점수 비교/score-comparison.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int am, ae, bm, be;
+    cin >> am >> ae >> bm >> be;
+
+    if(am > bm && ae > be) cout << 1;
+    else cout << 0;
+}
