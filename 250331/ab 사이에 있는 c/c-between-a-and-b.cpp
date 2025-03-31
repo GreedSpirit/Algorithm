e463c7e95056d4e7fbae7b01c674983c9c552df9
GreedSpirit <147044891+GreedSpirit@users.noreply.github.com>
2025-03-31 20:35:37 +0900

    Title: ab 사이에 있는 c, Time: 5ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250331/ab 사이에 있는 c/c-between-a-and-b.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+    int c;
+    cin >> c;
+
+    for(int i = a; i <= b; i++){
+        if(i % c == 0){
+            cout << "YES";
+            return 0;
+        }
+    }
+    cout << "NO";
+}
