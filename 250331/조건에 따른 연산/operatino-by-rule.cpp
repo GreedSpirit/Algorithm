e4d92870108464f4741286a24b98641868837e1a
GreedSpirit <147044891+GreedSpirit@users.noreply.github.com>
2025-03-31 20:31:13 +0900

    Title: 조건에 따른 연산, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250331/조건에 따른 연산/operatino-by-rule.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int cnt = 0;
+
+    while(n < 1000){
+        if(n % 2) n = n * 2 + 2;
+        else n = n * 3 + 1;
+        cnt++;
+    }
+    cout << cnt;
+}
