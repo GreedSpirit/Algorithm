da0d11adbb4a25ec25afe1ef4ab0e85b0109b4c0
GreedSpirit <147044891+GreedSpirit@users.noreply.github.com>
2025-04-30 11:17:24 +0900

    Title: 문자 개수 세기, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250430/문자 개수 세기/count-char.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string s;
+    getline(cin, s);
+
+    char c;
+    cin >> c;
+
+    int cnt = 0;
+
+    for(int i = 0; i < s.length(); i++){
+        if(s[i] == c) cnt++;
+    }
+    cout << cnt;
+}
