84844f7a6fb5c3765596d27d785e7b5240f7649f
GreedSpirit <147044891+GreedSpirit@users.noreply.github.com>
2025-05-07 12:25:04 +0900

    Title: 부분 문자열의 개수, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250507/부분 문자열의 개수/number-of-substrings.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string a, b;
+    cin >> a >> b;
+
+    int cnt = 0;
+
+    for(int i = 0; i < a.length() - 1; i++){
+        if(a.substr(i, 2) == b) cnt++;
+    }
+    cout << cnt;
+}
