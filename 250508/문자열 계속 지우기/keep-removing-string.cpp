a0bb1fc91b0acbae1abe1df8d63c2af5e56081d2
GreedSpirit <147044891+GreedSpirit@users.noreply.github.com>
2025-05-08 12:44:47 +0900

    Title: 문자열 계속 지우기, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250508/문자열 계속 지우기/keep-removing-string.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string a, b;
+    cin >> a >> b;
+
+    while(a.find(b) != string::npos){
+        int idx = a.find(b);
+        a.erase(idx, b.length());
+    }
+    cout << a;
+}
