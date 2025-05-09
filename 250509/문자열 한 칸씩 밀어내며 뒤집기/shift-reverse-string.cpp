d7e6b89aac5afd066bcd33ace5d9b942ca54ffe1
GreedSpirit <147044891+GreedSpirit@users.noreply.github.com>
2025-05-09 13:45:47 +0900

    Title: 문자열 한 칸씩 밀어내며 뒤집기, Time: 7ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250509/문자열 한 칸씩 밀어내며 뒤집기/shift-reverse-string.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string s;
+    int q;
+    cin >> s >> q;
+    string tmp = "";
+
+    while(q--){
+        int n;
+        cin >> n;
+        if(n == 1){
+            tmp = s.substr(1, s.length() - 1) + s[0];
+        }
+        else if(n == 2){
+            tmp = s[s.length() - 1] + s.substr(0, s.length() - 1);
+        }
+        else{
+            for(int i = 0; i < s.length(); i++){
+                tmp[i] = s[s.length() - 1 - i];
+            }
+        }
+        s = tmp;
+        cout << s << '\n';
+    }
+}
