f27518c170afb1d330bc55aae083b304d4bdeb51
GreedSpirit <147044891+GreedSpirit@users.noreply.github.com>
2025-05-14 09:16:26 +0900

    Title: 절댓값으로 변경, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

--- 250514/절댓값으로 변경/find-the-absolute-value.cpp
+++ 250514/절댓값으로 변경/find-the-absolute-value.cpp
@@ -1,16 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void fun(vector<int> v){
+void fun(vector<int>& v){
     for(int i = 0; i < v.size(); i++){
         v[i] = abs(v[i]);
     }
-    cout << v.size();
 }
 
 
 int main() {
     int n;
+    cin >> n;
     vector<int> v(n);
 
     for(int i = 0; i < n; i++){
