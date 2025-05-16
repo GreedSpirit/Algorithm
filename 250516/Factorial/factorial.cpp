8683634c1bb785876eecf7a6b76fe9fe6633fb8e
GreedSpirit <147044891+GreedSpirit@users.noreply.github.com>
2025-05-16 14:25:26 +0900

    Title: Factorial, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250516/Factorial/factorial.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int N;
+
+int f(int n){
+    if(n == 0) return 1;
+    if(n == 1) return 1;
+
+    return f(n - 1) * n;
+
+}
+
+int main() {
+    cin >> N;
+
+    cout << f(N);
+
+    // Please write your code here.
+
+    return 0;
+}
