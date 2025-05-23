450403ec936c38c3d570cbd7f952743602266c1f
GreedSpirit <147044891+GreedSpirit@users.noreply.github.com>
2025-05-23 14:19:41 +0900

    Title: 총점 비교, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250523/총점 비교/compare-total-points.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace  std;
+
+class student{
+    public:
+    string name;
+    int k;
+    int e;
+    int m;
+
+    student(string s, int i, int i2, int i3){
+        name = s;
+        k = i;
+        e = i2;
+        m = i3;
+    }
+
+    student(){
+
+    }
+};
+
+bool compare(const student& a, const student& b){
+    return a.k + a.e + a.m < b.k + b.e + b.m;
+}
+
+int main(){
+    int n;
+    cin >> n;
+
+    vector<student> v(n);
+
+    for(int i = 0; i < n; i++){
+        string s;
+        int i1, i2, i3;
+        cin >> s >> i1 >> i2 >> i3;
+        v[i] = student(s, i1, i2, i3);
+    }
+
+    sort(v.begin(), v.end(), compare);
+    
+    for(auto a : v){
+        cout << a.name << ' ' << a.k << ' ' << a.e << ' ' << a.m << '\n';
+    }
+
+
+}
