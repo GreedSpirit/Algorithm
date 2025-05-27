375ee90a7b213e0dd13b7c873267a5f9732dae8c
GreedSpirit <147044891+GreedSpirit@users.noreply.github.com>
2025-05-28 07:22:47 +0900

    Title: 2진수로 변환하기, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

--- 250528/2진수로 변환하기/convert-to-binary.cpp
+++ 250528/2진수로 변환하기/convert-to-binary.cpp
@@ -6,6 +6,11 @@ int main(void){
     cin >> n;
     vector<int> v;
 
+    if(n == 0){
+        cout << 0;
+        return 0;
+    }
+
     while(n > 0){
         v.push_back(n % 2);
         n = n / 2;
