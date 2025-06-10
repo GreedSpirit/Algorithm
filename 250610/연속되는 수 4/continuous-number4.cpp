3925e9abebac225b9046c214ce0bc544ea1aceff
GreedSpirit <147044891+GreedSpirit@users.noreply.github.com>
2025-06-10 22:47:27 +0900

    Title: 연속되는 수 4, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250610/연속되는 수 4/continuous-number4.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int ans = 0;
+    int n;
+    cin >> n;
+
+    vector<int> v;
+
+    int cnt = 0;
+
+    while(n--){
+        int x;
+        cin >> x;
+        v.push_back(x);
+    }
+
+    int tmp = v[0];
+
+    for(int a : v){
+        if(a <= tmp){
+            ans = max(ans, cnt);
+            cnt = 1;
+        }
+        else{
+            cnt++;
+        }
+        tmp = a;
+    }
+    ans = max(ans, cnt);
+    cout << ans;
+
+}
