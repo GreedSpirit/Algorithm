1164c56f7647633c43d8c4d711fc71554b10632c
GreedSpirit <147044891+GreedSpirit@users.noreply.github.com>
2025-09-18 18:04:19 +0900

    Title: 빙빙 돌며 숫자 사각형 채우기 2, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250918/빙빙 돌며 숫자 사각형 채우기 2/snail-number-square-2.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int dx[4] = {1, 0, -1, 0};
+int dy[4] = {0, 1, 0, -1};
+
+int N, M;
+
+bool IsRange(int x, int y){
+    return x >= 0 && x < N && y >= 0 && y < M;
+}
+
+int main(void){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> N >> M;
+
+    int arr[N][M] = {};
+
+    int x = 0;
+    int y = 0;
+    int dir = 0;
+
+    for(int i = 1; i <= N * M; i++){
+        arr[x][y] = i;
+        int nx = x + dx[dir];
+        int ny = y + dy[dir];
+
+        if(!IsRange(nx, ny) || arr[nx][ny] != 0){
+            dir = (dir + 1) % 4;
+        }
+
+        x = x + dx[dir];
+        y = y + dy[dir];
+
+    }
+
+    for(int i = 0; i < N; i++){
+        for(int j = 0; j < M; j++){
+            cout << arr[i][j] << ' ';
+        }
+        cout << '\n';
+    }
+
+}
