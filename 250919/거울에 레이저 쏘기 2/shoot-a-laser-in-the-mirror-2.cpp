188acfdd72adf58fc2778bebeb434f5cae54ec21
GreedSpirit <147044891+GreedSpirit@users.noreply.github.com>
2025-09-19 16:24:16 +0900

    Title: 거울에 레이저 쏘기 2, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 250919/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.cpp
+++ 250919/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int N;
 
 int dx[4] = {1, 0, -1, 0};
-int dy[4] = {0, -1, 0, 1};
+int dy[4] = {0, 1, 0, -1};
 
 bool IsRange(int x, int y){
     return x >= 0 && x < N && y >= 0 && y < N;
@@ -31,7 +31,18 @@ int main(void){
     cin >> K;
 
     int dir;
-    dir = (K - 1) / N;
+    if((K - 1) / N == 0){
+        dir = 0;
+    }
+    else if ((K - 1) / N == 1){
+        dir = 3;
+    }
+    else if((K - 1) / N == 2){
+        dir = 2;
+    }
+    else{
+        dir = 1;
+    }
 
     int x, y;
 
@@ -39,29 +50,42 @@ int main(void){
         x = 0;
         y = K - 1;
     }
-    else if(dir == 1){
+    else if(dir == 3){
         y = N - 1;
         x = (K - 1) - N;
     }
     else if(dir == 2){
         x = N - 1;
-        y = abs(3 * N - K);
+        y = 3 * N - K;
     }
     else{
         y = 0;
-        x = abs(4 * N - K);
+        x = 4 * N - K;
     }
 
     int ans = 0;
 
     while(true){
         if(arr[x][y] == '\\'){
-            ans++;
-            dir = (dir + 1) % 4;
+            if(dir == 0 || dir == 3){
+                ans++;
+                dir = (dir + 1) % 4;
+            }
+            else{
+                ans++;
+                dir = (dir + 3) % 4;
+            }
+            
         }
         else if(arr[x][y] == '/'){
-            ans++;
-            dir = (dir + 3) % 4;
+            if(dir == 0 || dir == 1){
+                ans++;
+                dir = (dir + 3) % 4;
+            }
+            else{
+                ans++;
+                dir = (dir + 1) % 4;
+            }
         }
 
         int nx = x + dx[dir];
