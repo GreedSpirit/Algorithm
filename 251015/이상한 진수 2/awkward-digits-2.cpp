704b64e64b27964cf862841db29c46753dd78b3c
GreedSpirit <147044891+GreedSpirit@users.noreply.github.com>
2025-10-15 16:24:24 +0900

    Title: 이상한 진수 2, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 251015/이상한 진수 2/awkward-digits-2.cpp
+++ 251015/이상한 진수 2/awkward-digits-2.cpp
@@ -7,6 +7,11 @@ string a;
 int main() {
     cin >> a;
 
+    bool iszero = false;
+    for(int i = 1; i < a.length(); i++){
+        if(a[i] == '0') iszero = true;
+    }
+
     for(int i = 1; i < a.length(); i++){
         if(a[i] == '0'){
             a[i] = '1';
@@ -19,6 +24,9 @@ int main() {
     int doublemulti = 1;
     for(int i = a.length() - 2; i >= 0; i--){
         ans += (a[i] - '0') * pow(2, (a.length() - 1 - i));
+    }
+    if(!iszero){
+        ans -= 1;
     }
     cout << ans;
 
