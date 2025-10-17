eae291a291092cd73e7c98c486cbda9b8998bd4f
GreedSpirit <147044891+GreedSpirit@users.noreply.github.com>
2025-10-17 17:30:15 +0900

    Title: 괄호 쌍 만들어주기 2, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251017/괄호 쌍 만들어주기 2/pair-parentheses-2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string A;
+
+int main() {
+    cin >> A;
+    int ans = 0;
+
+    for(int i = 0; i < A.length() - 3; i++){
+        if(A[i] == '(' && A[i + 1] == '('){
+            for(int j = i + 2; j < A.length() - 1; j++){
+                if(A[j] == ')' && A[j + 1] == ')'){
+                    ans++;
+                }
+            }
+        }
+    }
+    cout << ans;
+    return 0;
+}
