0cc4613c24e6536bbb45502efe2e03ad6d75515c
GreedSpirit <147044891+GreedSpirit@users.noreply.github.com>
2025-11-17 17:03:20 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 56 ms, Memory: 2156 KB -BaekjoonHub

---
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.cc
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int N , K , len = 0;
+    cin >> N >> K;
+    
+    int pre[5001];
+    int nxt[5001];
+    
+    vector<int> v;
+    
+    for(int i = 1; i <= N; i++){
+        pre[i] = (i == 1) ? N : i - 1;
+        nxt[i] = (i == N) ? 1 : i + 1;
+    }
+    len = N;
+    
+    int i = 1;
+    
+    for(int cur = 1; len != 0; cur = nxt[cur]){
+        if(i == K){
+            pre[nxt[cur]] = pre[cur];
+            nxt[pre[cur]] = nxt[cur];
+            v.push_back(cur);
+            i = 1;
+            len--;
+        }
+        else ++i;
+    }
+    
+    cout << "<";
+    for(int j = 0; j < v.size(); ++j){
+        cout << v[j];
+        if(j != v.size() - 1) cout << ", ";
+    }
+    cout << ">";
+    
+    
+    
+    
+    
+}
